5a8c0cb70c4b20e70c480f22147a4650fb15971f
Daniel Calvo <vioseven@gmail.com>
2017-12-25 16:43:56 +0100

    Book reading

--- Python/Tutorial/Chapter18.py
+++ Python/Tutorial/Chapter18.py
@@ -54,8 +54,53 @@ L = [1,2]
 X, L = multiple(X, L)
 print(X, L)
 
+#Simple function with 3 arguments
+
+def fu(a,b,c): print(a,b,c)
+fu(6,6,6)
+
+#Oh, we can match by name!
+
+fu(c=8,b=7,a=6)
+
+#You can mix and mash the two:
+fu(1,c=3,b=3)
+
+#Creating a function with defaults arguments if not enough ones are passed:
+
+def fu1(a, b=2, c=3): print(a,b,c)
+
+fu1(7)
+fu1(7,1)
+fu1(4,c=7)
+
+def fu2(spam, eggs, toast=0, ham=0): print(spam, eggs, toast, ham)
+fu2(1,2)
+
+#Take as many arguments as you want!
+
+def fu3(*args): print(args) #Tuple!
+fu3(235,2345,435,6577)
+
+def fu4(**args): print(args) #Only works for keyword arguments, makes a dictionary
+a = fu4(a=4,aaa=666)
+
+def fu5(a, *pargs, **kargs): print (a, pargs, kargs)
+
+fu5(1,2,3, x=1, y=2)
+
+def fu6(a,b,c,d): print(a,b,c,d)
+myargs = (1,2)
+myargs += (3,4) #It's a tuple!
+
+fu6(*myargs)
+
+myargs = {'a': 1, 'b': 2, 'c': 3}
+myargs['d'] = 4
+
+fu6(**myargs)
+
+fu6(*(1,2), **{'d': 44, 'c': 33})
 
 
-
-
-
+#That tracer function looks cool!
