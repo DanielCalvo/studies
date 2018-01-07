1c8f56c99b460d686f8a8ad1c4b16f4b07418585
Daniel Calvo <vioseven@gmail.com>
2018-01-08 00:04:54 +0100

    Studying

---
+++ Python/Corey's youtube tutorials/json_tutorial.py
@@ -0,0 +1,21 @@
+
+import json
+from urllib.request import urlopen
+
+#
+# #
+# with open('states.json') as f:
+#     data = json.load(f)
+# #
+# for state in data['states']:
+#     del state['area_codes']
+#
+# with open('new_states.json', 'w') as f1:
+#     json.dump(data, f1, indent=2)
+
+response = urlopen('http://a.4cdn.org/n/catalog.json')
+
+data = json.loads(response.read())
+print(json.dumps(data, indent=2))
+
+#https://www.youtube.com/watch?v=9N6a-VLBa2I
