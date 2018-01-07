76fab23d426965c3c57b58e59553e2fa62c48fdb
Daniel Calvo <vioseven@gmail.com>
2018-01-07 15:25:11 +0100

    Adding my cool image resizing program to this thing

--- Python/Corey's youtube tutorials/webscrapping.py
+++ Python/Corey's youtube tutorials/webscrapping.py
@@ -4,52 +4,83 @@
 
 #wget https://www.milanuncios.com/motos-de-segunda-mano-en-baleares/?desde=500
 
+from fake_useragent import UserAgent
+ua = UserAgent()
 from bs4 import BeautifulSoup
 import requests
 
-#source = requests.get('https://www.milanuncios.com/motos-de-segunda-mano-en-baleares/?desde=500').text
-#soup = BeautifulSoup(source, 'lxml')
+headers = {ua.google}
+print(headers)
+
+
+#
+#
+#source = requests.get('https://www.milanuncios.com/motos-de-segunda-mano-en-baleares/?desde=500', ua.google).text
+source = requests.get('https://www.milanuncios.com', ua.google).text
+# soup = BeautifulSoup(source, 'lxml')
+print(source)
+print(source.sta)
 
 #Using the file manually for now as milanuncios doesn't allow robots:
 
-with open('milanuncios.html') as html_file:
-    soup = BeautifulSoup(html_file, 'lxml')
+# with open('milanuncios.html') as html_file:
+#     soup = BeautifulSoup(html_file, 'lxml')
 
 #ad = soup.find('div', class_='aditem')
 #print(ad.prettify())
 
 
-for ad in soup.find_all('div', class_='aditem'):
+# for ad in soup.find_all('div', class_='aditem'):
+#
+#     try:
+#         price = ad.find('div', class_='aditem-price').text
+#         print(price)
+#     except AttributeError:
+#         price = 'N/A'
+#
+#     try:
+#         displacement = ad.find('div', class_='cc tag-mobile').text
+#         print(displacement)
+#     except AttributeError:
+#         displacement = 'N/A'
+#
+#     try:
+#         description = ad.find('div', class_='tx').text
+#         print(description)
+#     except AttributeError:
+#         description = 'N/A'
+#
+#     try:
+#         year = ad.find('div', class_='ano tag-mobile').text
+#         print(year)
+#     except AttributeError:
+#         year = 'N/A'
+#
+#     try:
+#         advert_id = ad.find('div', class_='x5').text.strip()
+#         print(advert_id)
+#     except AttributeError:
+#         advert_id = 'N/A'
+#
+#     try:
+#         location = ad.find('div', class_='x4').text
+#         location = location.split(' ')
+#         location = location[-1]
+#         print(location)
+#     except AttributeError:
+#         location = 'N/A'
+#
+#     try:
+#         time_since_last_renewed = ad.find('div', class_='x6').text
+#         print(time_since_last_renewed)
+#     except AttributeError:
+#         time_since_last_renewed = 'N/A'
 
-     price = ad.find('div', class_='aditem-price').text
-     print(price)
-
-     displacement = ad.find('div', class_='cc tag-mobile').text
-     print(displacement)
-
-     description = ad.find('div', class_='tx').text
-     print(description)
 
     #TODO: Try & catch so that the program doesn't crash when a given field is missing!
     #TODO: https://www.youtube.com/watch?v=ng2o98k983k&index=43&t=2305s&list=PL-osiE80TeTt2d9bfVyTiXJA-UTHn6WwU
 
-     year = ad.find('div', class_='ano tag-mobile').text
-     print(year)
 
-
-#
-#     location = ad.find('div', class_='x4').text
-#     location = location.split(' ')
-#     location = location[-1]
-#     print(location)
-#
-#     advert_id = ad.find('div', class_='x5').text.strip()
-#     print(advert_id)
-#
-#     time_since_last_renewed = ad.find('div', class_='x6').text
-#     print(time_since_last_renewed)
-#
-#
 
 #TODO: Kilometers. Can't do it on the current example as this guy doesn't have the mileage available on the Burgman.
 #TODO: How do you handle exceptions when a given field isn't avaialble?
---
+++ Python/apps/image_resizer.py
@@ -0,0 +1,31 @@
+
+#Simple and crude program that takes images from one directory, resizes and saves them into another directory
+
+from PIL import Image
+import os
+
+image_directory = 'E:\Daniel O RETORNO\Imagens\mobile pix'
+resized_image_directory = image_directory + '\\resized'
+image_list = [f for f in os.listdir(image_directory) if f.endswith('.jpg') or f.endswith('jpg')]
+
+try:
+    os.stat(resized_image_directory)
+except FileNotFoundError:
+    os.mkdir(resized_image_directory)
+
+for image_file in image_list:
+    try:
+        os.stat(resized_image_directory+'\\'+image_file)
+        print(image_file+' is already resized. Skipping')
+        continue
+    except FileNotFoundError:
+        pass
+    try:
+        im = Image.open(image_directory+'\\'+image_file)
+    except OSError:
+        print('Could not open'+image_directory+'\\'+image_file+'. Is it a valid image?')
+        continue
+    if im.size == ((4864, 2736)) or im.size == ((2592, 1456)): #I'm only interested in resizing images that match these two sizes.
+        im_resized = im.resize((1920,1080))
+        im_resized.save(resized_image_directory+'\\'+image_file)
+        print(image_file+' was resized')
