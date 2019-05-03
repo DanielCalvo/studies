b8fe51972de3aa4f8e26b09e47dadf0b090e00b1
Daniel Calvo <vioseven@gmail.com>
2019-05-03 23:01:16 +0200

    Learning Go

---
+++ Golang/GoTheCompleteDevelopersGuide/Section5_Maps/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+
+	//var colors map[string]string
+
+	//colors := make(map[string]string)
+	//colors["white"] = "#ffffff"
+	//delete(colors, "white")
+
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "00ff00",
+		"white": "ffffff",
+	}
+
+	//fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
