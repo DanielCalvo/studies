171107c207bcf6d7a48c5e9a7435e7b99f7695ca
Daniel Calvo <daniel.calvo@trivago.com>
2019-05-17 14:18:34 +0200

    Learning Go

--- Golang/LearnHowToCodeGooglesGolangProgrammingLanguage/Section4/main.go
+++ Golang/LearnHowToCodeGooglesGolangProgrammingLanguage/Section4/main.go
@@ -9,19 +9,34 @@ var y = 22
 //Assigns the zero value of type int to z
 //zero value for its type: false for booleans, 0 for numeric types, "" for strings, and nil for pointers, functions, interfaces, slices, channels, and maps.
 var z int
+var a string = `This is a raw string.
+%T "banana"`
+
+type hotdog int
+
+var b hotdog
 
 func main() {
 	n, err := fmt.Println("Hello world")
 	fmt.Println(n, err)
 
-	x := 42
-	fmt.Println(x)
-	x = 43
-	fmt.Println(x)
+	//x := 42
+	//fmt.Println(x)
+	//x = 43
+	//fmt.Println(x)
 	//y := 100 + 24
-	fmt.Println(y)
+	//fmt.Println(y)
+	fmt.Printf("Variable y is of type: %T\n", y)
+	fmt.Println(a) //Includes line breaks!
 
-	fmt.Println(y)
+	fmt.Printf("%T\n", y)
+	fmt.Printf("%b\n", y)
+	fmt.Printf("%x\n", y)
+	fmt.Printf("%#x\n", y)
+	fmt.Printf("%T\n", b)
+	myvar := int(b)
+	fmt.Println(myvar)
+
 }
 
 func foo() {
