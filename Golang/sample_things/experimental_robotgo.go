e274b861cb5da647b65ca767899ca63248adcc62
DanielCalvo <vioseven@gmail.com>
2019-06-21 09:44:03 +0200

    Experimenting with robotgo

--- Golang/sample_things/experimental_robotgo.go
+++ Golang/sample_things/experimental_robotgo.go
@@ -10,7 +10,13 @@ func main() {
 	//Takes and saves a screenshot, neat!
 	bitmap := robotgo.CaptureScreen()
 	fmt.Println("...", bitmap)
-	robotgo.SaveBitmap(bitmap, "/home/daniel/PycharmProjects/studies/Golang/sample_things/test.png")
+	robotgo.SaveBitmap(bitmap, "/tmp/test.png")
+
+	//We need to get a sub bitmap from somewhere
+
+	robotgo.OpenBitmap()
+
+	robotgo.FindBitmap(bitmap)
 
 	//TODO next: Find a subimage of my screenshot. (feed another image into the program, find it inside the screenshot, get it's coordinates)
 
