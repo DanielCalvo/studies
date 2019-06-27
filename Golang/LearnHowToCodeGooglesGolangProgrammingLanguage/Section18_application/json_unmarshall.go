6f9a0e41e159bd0824ebefc54ef8e9968afb56ed
Daniel Calvo <vioseven@gmail.com>
2019-06-27 08:52:22 +0200

    Udemy Golang course

--- Golang/LearnHowToCodeGooglesGolangProgrammingLanguage/Section18_application/json_unmarshall.go
+++ Golang/LearnHowToCodeGooglesGolangProgrammingLanguage/Section18_application/json_unmarshall.go
@@ -1,11 +1,34 @@
 package main
 
-type myperson struct {
-	First string `json:"First"`
-	Last  string `json:"Last"`
-	Age   int    `json:"Age"`
+import (
+	"encoding/json"
+	"fmt"
+)
+
+//http://api.open-notify.org/astros.json
+
+type astronauts struct {
+	People []struct {
+		Craft string `json:"craft"`
+		Name  string `json:"name"`
+	} `json:"people"`
+	Number  int    `json:"number"`
+	Message string `json:"message"`
 }
 
 func main() {
+
+	myjson := `{"people": [{"craft": "ISS", "name": "Alexey Ovchinin"}, {"craft": "ISS", "name": "Nick Hague"}, {"craft": "ISS", "name": "Christina Koch"}], "number": 3, "message": "success"}`
+	myjson_bytes := []byte(myjson)
+	astronauts1 := astronauts{}
+
+	json.Unmarshal(myjson_bytes, &astronauts1)
+
+	fmt.Println("Did it work?")
+	fmt.Println(astronauts1)
+
+	for _, v := range astronauts1.People {
+		fmt.Println(v)
+	}
 
 }
