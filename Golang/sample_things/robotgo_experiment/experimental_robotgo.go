738418b537b384368c9be55eb6a986d0559e905b
DanielCalvo <vioseven@gmail.com>
2019-06-27 22:54:11 +0200

    GUI automation with GoLang

--- Golang/sample_things/robotgo_experiment/experimental_robotgo.go
+++ Golang/sample_things/robotgo_experiment/experimental_robotgo.go
@@ -6,8 +6,8 @@ import (
 	//"time"
 	//"bytes"
 	//"log"
-	"fmt"
 	"github.com/go-vgo/robotgo"
+	"log"
 	"math/rand"
 	"os"
 	"time"
@@ -78,7 +78,7 @@ func put_life_ring_on() {
 	empty_ring_slot_x, empty_ring_slot_y := robotgo.FindBitmap(empty_ring_slot, robotgo.CaptureScreen(), 0.1)
 
 	if empty_ring_slot_x == -1 || empty_ring_slot_y == -1 {
-		fmt.Println("Life ring slot in use")
+		log.Println("Life ring slot in use")
 		return
 	}
 
@@ -86,12 +86,12 @@ func put_life_ring_on() {
 	life_ring_x, life_ring_y := robotgo.FindBitmap(life_ring, robotgo.CaptureScreen(), 0.0)
 
 	if life_ring_x == -1 || life_ring_y == -1 {
-		fmt.Println("No life ring found")
+		log.Println("No life ring found")
 		return
 	}
 
-	fmt.Println("Life ring: ", life_ring_x, life_ring_y)
-	fmt.Println("empty ring slot: ", empty_ring_slot_x, empty_ring_slot_y)
+	log.Println("Life ring: ", life_ring_x, life_ring_y)
+	log.Println("empty ring slot: ", empty_ring_slot_x, empty_ring_slot_y)
 
 	robotgo.MoveMouseSmooth(life_ring_x, life_ring_y)
 	time.Sleep(500 * time.Millisecond)
@@ -118,26 +118,42 @@ func check_blank_rune() {
 	blank_rune_x, blank_rune_y := robotgo.FindBitmap(blank_rune, robotgo.CaptureScreen(), 0.1)
 
 	if blank_rune_x == -1 || blank_rune_y == -1 {
-		fmt.Println("Blank rune not found, runemaking not possible, exiting")
+		log.Println("Blank rune not found, runemaking not possible, exiting")
 		os.Exit(1)
 	} else {
-		fmt.Println("Found a blank rune at", blank_rune_x, blank_rune_y)
+		log.Println("Found a blank rune at", blank_rune_x, blank_rune_y)
 	}
 }
 
 func use_manapot() {
-	manapot := robotgo.OpenBitmap(image_directory + "manapot.png.png")
+	manapot := robotgo.OpenBitmap(image_directory + "manapot.png")
+	time.Sleep(500 * time.Millisecond)
 	manapot_x, manapot_y := robotgo.FindBitmap(manapot, robotgo.CaptureScreen(), 0.1)
 	if manapot_x == -1 || manapot_y == -1 {
-		fmt.Println("Manapot not found")
+		log.Println("Manapot not found")
 		return
 	}
 	robotgo.MovesClick(manapot_x, manapot_y, "right")
 	robotgo.MovesClick(character_center_screen_x, character_center_screen_y)
 }
 
+func manabar_status(manabar_x int, manabar_y int) (status string) {
+	manabar_color := robotgo.GetPixelColor(manabar_x, manabar_y)
+
+	if manabar_color == "00469b" {
+		return "full"
+	}
+
+	if manabar_color == "2a2b2a" {
+		return "empty"
+	}
+
+	return "invalid" //if could n
+
+}
+
 var use_life_ring bool = true
-var use_mana_pot bool = false
+var use_mana_pot bool = true
 var mana_pot_interval = 0
 
 //Set these up before running the program
@@ -156,36 +172,48 @@ func main() {
 	//session_value_generated := 0
 	manabar_full_counter := 0
 
+	//TODO: Track value generated properly
+	//Track money spent as well (increase numbers every ttime you drink a manapot or move a life ring)
+
+	// - - - - -  RUNE TYPE!
+	rune_type := Energy_bomb.magic_words
+	// - - - - -  RUNE TYPE!
+
 	for {
-		check_blank_rune()
+		//check_blank_rune()
+		//always exit here , don't exit in the function
 
 		if use_life_ring == true {
 			put_life_ring_on()
 		}
 
-		manabar_color := robotgo.GetPixelColor(manabar_x, manabar_y)
-
-		if manabar_color == "00469b" {
-			fmt.Println("Manabar full")
-			if manabar_full_counter > 10 {
-				fmt.Println("Mana full for more than 10 times in a row, something's wrong")
+		if man_stat := manabar_status(manabar_x, manabar_y); man_stat == "full" {
+			log.Println("manabar full")
+			if manabar_full_counter >= 5 {
+				log.Println("Manabar full five times in a row, something's wrong")
+				//Exit Tibia here
 				os.Exit(1)
 			}
 			eat(food_slot_x, food_slot_y)
-			make_rune(Energy_bomb.magic_words)
+			make_rune(rune_type)
 			manabar_full_counter += 1
-		} else if manabar_color == "2a2b2a" {
-			fmt.Println("Manabar empty")
+		}
+
+		if man_stat := manabar_status(manabar_x, manabar_y); man_stat == "empty" {
+			log.Println("Manabar empty")
 			manabar_full_counter = 0
 			if use_mana_pot == true {
 				use_manapot()
 			}
-		} else {
-			fmt.Println("Tibia not open or misaligned")
 		}
-		fmt.Println("manabar_full_counter:", manabar_full_counter)
-		fmt.Println("Sleeping 10")
-		time.Sleep(10 * time.Second)
+
+		if man_stat := manabar_status(manabar_x, manabar_y); man_stat == "invalid" {
+			log.Println("Tibia not open or misaligned")
+		}
+
+		log.Println("manabar_full_counter:", manabar_full_counter)
+		log.Println("Sleeping 5")
+		time.Sleep(5 * time.Second)
 	}
 
 	//Things done: Set up the counters
@@ -196,7 +224,7 @@ func main() {
 	//###
 	//manabar_full := robotgo.OpenBitmap("/home/daniel/PycharmProjects/studies/Golang/sample_things/manabar_full.png")
 	//manabar_square_12x12 := robotgo.OpenBitmap("/home/daniel/PycharmProjects/studies/Golang/sample_things/manabar_square_12x12.png")
-	//fmt.Println(robotgo.FindEveryBitmap(manabar_square_12x12, manabar_full, 0.1))
+	//log.Println(robotgo.FindEveryBitmap(manabar_square_12x12, manabar_full, 0.1))
 	////Move mouse to a part of the screen:
 	//xfce_x, xfce_y := robotgo.FindBitmap(xfce_menu_bitmap, robotgo.CaptureScreen(), 0.1)
 	//robotgo.MovesClick(xfce_x, xfce_y)
