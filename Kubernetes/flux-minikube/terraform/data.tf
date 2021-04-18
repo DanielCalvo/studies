e15040d21b174c810bf15e6ab179c03da04b3e46
DanielCalvo <vioseven@gmail.com>
2021-04-18 18:44:33 +0200

    flux + minikube shenanigans

--- Kubernetes/flux-minikube/terraform/data.tf
+++ Kubernetes/flux-minikube/terraform/data.tf
@@ -1,12 +1,12 @@
 
 # https://github.com/fluxcd/terraform-provider-flux/blob/main/docs/data-sources/install.md
 data "flux_install" "main" {
-  target_path = var.target_path
+//  target_path = var.target_path
   target_path = "../clusters/justmemeing"
 }
 # https://github.com/fluxcd/terraform-provider-flux/blob/main/docs/data-sources/sync.md
 data "flux_sync" "main" {
-  target_path = var.target_path
+//  target_path = var.target_path
   target_path = "../clusters/justmemeing"
   url         = "ssh://git@github.com/${var.github_owner}/${var.repository_name}.git"
   branch      = var.branch
