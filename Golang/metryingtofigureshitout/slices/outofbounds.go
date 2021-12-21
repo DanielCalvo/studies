da406304a008fa3d1eda01a0d00cb50b5cdee2cb
DanielCalvo <vioseven@gmail.com>
2021-12-21 23:59:42 +0100

    Some Golang

---
+++ Golang/metryingtofigureshitout/slices/outofbounds.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	//s := []string{"a", "b", "c"}
+	//fmt.Println(isOutOfBounds(s, 0))
+	//fmt.Println(isOutOfBounds(s, 4))
+
+	g := [][]string{
+		[]string{"a", "b", "c"},
+		[]string{"d", "e", "f"},
+		[]string{"g", "h", "i"},
+	}
+	fmt.Println(g[1][0])
+	fmt.Println(len(g), len(g[0]))
+	fmt.Println(isOutofBoundsMatrix(g, 3, 2))
+	recover()
+}
+
+//Let's assume a square matrix for now
+// y is the outer slice
+// x is the outer slice
+func isOutofBoundsMatrix(s [][]string, y, x int) bool {
+	if y < 0 || y > len(s)-1 {
+		return true
+	}
+	//For simplicity, I'm assuming all slices inside the slice have equal length (this matrix is either a square or a rectangle)
+	if x < 0 || x > len(s[0])-1 {
+		return true
+	}
+	return false
+}
+
+//Oh hey this could be a nice candidate for an interface for matrix/slice
+func isOutOfBounds(s []string, pos int) bool {
+	if pos < 0 || pos > len(s)-1 { //slices start at 0
+		return true
+	}
+	return false
+}
--- Golang/simple_things/grid_shenanigans/grid.go
+++ Golang/simple_things/grid_shenanigans/grid.go
@@ -3,63 +3,19 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"
 )
 
-type Cell struct {
-	x     int
-	y     int
-	value string
-}
-
-//Let's display a big X on screen on a 20x20 grid!
-//But first, let's initialize a bunch of cells
-
-func main() {
-
-	//cell := Cell{
-	//	x:     0,
-	//	y:     0,
-	//	value: "X",
-	//}
-
-	//cells := make([]Cell, 6)
-	//cells = populateCells(cells)
-	//fmt.Println(cells)
-	//print20x20()
-	//a := createSlice(5)
-	//fmt.Println(a)
-
-	b := createGrid(60, 60) //only accepts x == y, breaks otherwise, fix it later!
-	b = populateGrid(b)
-	b[0][0] = "a"
-	b[1][0] = "b"
-	b[2][0] = "c"
-	printGrid(b)
-	b = moveGridDown(b)
-	b = randomizeFirstLine(b)
-	printGrid(b)
-
-	//
-	for {
-		fmt.Print("\033[H\033[2J")
-		b = moveGridDown(b)
-		b = randomizeFirstLine(b)
-		printGrid(b)
-		time.Sleep(100 * time.Millisecond)
-	}
+type Coordinates struct {
+	x int
+	y int
 }
 
 func randomizeFirstLine(g [][]string) [][]string {
 	for i := 0; i < len(g[0]); i++ {
 		g[0][i] = getRandomString(1)
 	}
-
 	return g
 }
-
-//YOU STOPPED HERE: https://go.dev/blog/slices-intro
-//There's something wrong in this function. Changing the first line changes actually the first two lines.
 
 //Let's just one row for now, you can parametrize later
 //If you don't use copy() here and instead do `g[i] = g[i-1]` you run into some shallow copy problems and have 2 indexes pointing to the same thing
@@ -67,13 +23,6 @@ func moveGridDown(g [][]string) [][]string {
 	for i := len(g) - 1; i > 0; i-- {
 		copy(g[i], g[i-1])
 	}
-	return g
-}
-
-func randomizeMatrixStyle(g [][]string) [][]string {
-	//move the entire grid down one row
-	//generate a row of random characters
-	//insert that row in the top of the grid
 	return g
 }
 
@@ -93,23 +42,6 @@ func getRandomString(n int) string {
 		s[i] = letters[rand.Intn(len(letters))]
 	}
 	return string(s)
-}
-
-func populateCells(cell []Cell) []Cell {
-	//What does this number even mean? What do you call it?
-	num := len(cell) / 2 //But then we can only handle an even ammount of cells? uh-oh we need to handle this
-	var cs []Cell
-	for i := 0; i < num; i++ {
-		for j := 0; j < num; j++ {
-			c := Cell{
-				x:     i,
-				y:     j,
-				value: "a",
-			}
-			cs = append(cs, c)
-		}
-	}
-	return cs
 }
 
 func printGrid(g [][]string) {
