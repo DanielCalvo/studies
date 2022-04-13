4b0e933558c1133a06e6ceab82c490703763b0dc
DanielCalvo <vioseven@gmail.com>
2022-04-13 23:39:41 +0200

    Wrapping up my humble attempt at a crud api

--- Golang/simple_things/api_rest_postgres/main.go
+++ Golang/simple_things/api_rest_postgres/main.go
@@ -10,9 +10,8 @@ import (
 	"net/http"
 )
 
-type server struct{}
-
 type Company struct {
+	Id         int
 	Name       string
 	Valuation  float64
 	DateJoined string
@@ -33,56 +32,111 @@ func init() {
 }
 
 func main() {
-	//s := &server{}
-	//http.Handle("/", s)
-	//log.Fatal(http.ListenAndServe(":8080", nil))
 	r := mux.NewRouter()
 	api := r.PathPrefix("/api/v1").Subrouter()
 	api.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "api v1")
 	})
-	//...
-	api.HandleFunc("/companies/name/{name}", searchByName).Methods(http.MethodGet)
 
+	api.HandleFunc("/companies/name/{name}", searchByName).Methods(http.MethodGet)
+	api.HandleFunc("/company", addCompany).Methods(http.MethodPost)
+	api.HandleFunc("/companies/name/{name}", deleteByName).Methods(http.MethodDelete)
+	api.HandleFunc("/companies/name/{name}", updateByName).Methods(http.MethodPut)
 	log.Fatalln(http.ListenAndServe(":8080", r))
 
 }
 
-var id int
-var name, valuation, datejoined string
-
-//http://localhost:8080/api/v1/companies/name/SpaceX
-//select * from company where name = 'SpaceX';
+/*
+Case sensitive:
+curl -X GET http://localhost:8080/api/v1/companies/name/SpaceX
+*/
 func searchByName(w http.ResponseWriter, r *http.Request) {
 	var c Company
 
 	pathParams := mux.Vars(r)
 	if name, ok := pathParams["name"]; ok {
-		fmt.Println("Got name:", name)
 		row := db.QueryRow("SELECT * FROM company WHERE name = $1", name)
 
-		err := row.Scan(&id, &c.Name, &c.Valuation, &c.DateJoined)
+		err := row.Scan(&c.Id, &c.Name, &c.Valuation, &c.DateJoined)
 		switch {
 		case err == sql.ErrNoRows:
+			log.Println("Company not found in db:", pathParams["name"])
 			http.NotFound(w, r)
 			return
 		case err != nil:
-			fmt.Println("Error querying DB:", err)
+			log.Println("Error querying DB:", err)
 			http.Error(w, http.StatusText(500), http.StatusInternalServerError)
 			return
 		}
 		b, err := json.Marshal(c)
 		if err != nil {
-			fmt.Println("Error marshalling json:", err)
+			log.Println("Error marshalling json:", err)
 			http.Error(w, http.StatusText(500), http.StatusInternalServerError)
 		}
-
 		fmt.Fprintln(w, string(b))
+		log.Println("Search successful for:", c.Name)
 	}
 }
 
-//func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-//	w.Header().Set("Content-Type", "application/json")
-//	w.WriteHeader(http.StatusOK)
-//	w.Write([]byte(`{"message": "hello world"}`))
-//}
+/*
+curl -X POST http://localhost:8080/api/v1/company -H 'Content-Type: application/json' -d '{"Name":"memecorp","Valuation":1.1, "DateJoined":"2002/12/01"}'
+*/
+func addCompany(w http.ResponseWriter, r *http.Request) {
+	var c Company
+	err := json.NewDecoder(r.Body).Decode(&c)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	_, err = db.Exec("INSERT INTO company (name, valuation, datejoined) VALUES ($1, $2, $3)", c.Name, c.Valuation, c.DateJoined)
+	if err != nil {
+		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		log.Println("Error inserting into db:", err)
+		return
+	}
+	log.Println("Added to database:", c)
+
+}
+
+/*
+curl -X DELETE http://localhost:8080/api/v1/companies/name/memecorp
+*/
+func deleteByName(w http.ResponseWriter, r *http.Request) {
+	queries := mux.Vars(r)
+	name, ok := queries["name"]
+	if ok {
+		_, err := db.Exec("DELETE FROM company WHERE name=$1;", name)
+		if err != nil {
+			http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+			return
+		}
+		log.Println("Deleted company:", name)
+	}
+}
+
+/*
+Generally, in practice, use PUT for UPDATE operations. Always use POST for CREATE operations
+curl -X PUT http://localhost:8080/api/v1/companies/name/memecorp -H 'Content-Type: application/json' -d '{"Name":"memecorp","Valuation":2.2, "DateJoined":"2002/12/01"}'
+*/
+func updateByName(w http.ResponseWriter, r *http.Request) {
+	var c Company
+	err := json.NewDecoder(r.Body).Decode(&c)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	log.Println("update called!")
+	queries := mux.Vars(r)
+	name, ok := queries["name"]
+	fmt.Println(name, ok)
+	if ok {
+		_, err := db.Exec("UPDATE company SET name=$1, valuation=$2, datejoined=$3 WHERE name=$1;", c.Name, c.Valuation, c.DateJoined)
+		if err != nil {
+			log.Println("Error updating company:", err)
+			http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+			return
+		}
+	}
+	log.Println("Updated:", name, "with the following:", c)
+}
--- Golang/simple_things/api_rest_postgres/notes.md
+++ Golang/simple_things/api_rest_postgres/notes.md
@@ -1,5 +1,11 @@
 ## Ah
 I was going to do a full CRUD api, but dealing with postgreSQL got kinda boring, there's not much of a challenge here
+- How do you do pagination if someone wants a list of all the companies?
+    - Can you return all the companies that start with the letter "H" or have a valuation bigger than X billion?
+- How do you do authentication so that not anyone can delete companies?
+- What if the DB stops responding?
+
+Dang this rabbit hole goes deep
 
 ---
 
@@ -29,3 +35,8 @@ Fields:
 - Investors Count 
 - Deal Terms 
 - Portfolio Exits
+
+## Other ideas
+- How do apply the "Cloud native Go" book to this?
+- Can you implement some sort of authentication? 
+- Everyone uses Postman these days, I should probably learn to use that instead of curl and browser...
