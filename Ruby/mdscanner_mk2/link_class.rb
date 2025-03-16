5f7a1e360d46b0b7cb69d79b7a711339148ce382
Daniel Calvo <vioseven@gmail.com>
2025-03-16 19:49:31 +0100

    brief ruby file

--- Ruby/mdscanner_mk2/link_class.rb
+++ Ruby/mdscanner_mk2/link_class.rb
@@ -16,3 +16,19 @@ last checked
 methods:
 check link
 =end
+
+
+#Don't foger to documenet the class later
+class MarkdownLink
+  def initialize(text:, address:, local_file_path:, http_file_path:)
+    #Skipping filename as on my go program
+    @text = text
+    @address = address
+    @local_file_path = local_file_path
+    @http_file_path = http_file_path
+    @type = "UNKNOWN" #Figure this out by calling a private method to get the link type! According to chatgpt calling a method inside initialize is idiomatic and all!
+    @status = "UNKNOWN"
+  end
+end
+
+#maybe you can create a function to get the file name to replace the "filename" class variable you had in go
